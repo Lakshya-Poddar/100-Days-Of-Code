d28eba3da8b2e9c5c0b39d1a6f88d4fdf61f3a60
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-02-06 22:52:45 +0530

    props
    30 DAYS  OF CODE COMPLETE

---
+++ DAY 30/helloworld/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Greet(){
+//     return <h1>HELLO LAKSHYA</h1>
+// }
+
+const Greet=(props)=><h1>HELLO {props.name}</h1>
+
+export default Greet
---
+++ DAY 30/helloworld/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React ,{Component} from 'react';
+
+class Welcome extends Component{
+    render(){
+        return <h1>HELLO LAKSHYA FROM CLASS</h1>
+    }
+}
+
+export default Welcome
