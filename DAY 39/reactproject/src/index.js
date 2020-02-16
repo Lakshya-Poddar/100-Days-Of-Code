903e04f118c2db900bdc5d51c7ebc1900adbc2d5
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-02-16 17:41:16 +0530

    Update index.js

--- DAY 39/reactproject/src/index.js
+++ DAY 39/reactproject/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Avatar from './Avatar'
-ReactDOM.render(<div><Avatar id='1' name='lakshya' work='webdeveloper' /><Avatar /><Avatar /><Avatar /></div>, document.getElementById('root'));
+ReactDOM.render(<div><Avatar/></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
