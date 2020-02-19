f9216933605f761e5ff5cb00c903f00a968ec51d
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-02-19 23:21:29 +0530

    Routing and creating context Beach Resort Project

---
+++ DAY 43/hotelapp/src/context.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+const RoomContext=React.createContext();
+//<RoomContext.Provider value={'hello'}
+export class RoomProvider extends Component {
+    state={
+
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default context
