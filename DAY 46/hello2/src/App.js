eaf08df642e211425f9b076db3d9b5157a07ce8d
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-02-22 19:51:11 +0530

    Working With Props

---
+++ DAY 46/hello2/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Props from './Props';
+function App() {
+
+  return (
+    <div>
+      <Props name="LAKSHYA" email="xyz@gmail.com" phno="XXXXXXXXX"></Props>
+      <Props name="MUKUND" email="xyz2@gmail.com" phno="XXX-XXX-XXX"></Props>
+      <Props name="UNKNOWN" email="unknown@gmail.com" phno="12345687890"></Props>
+      <Props name="XYZ" email="xyz3@gmail.com" phno="000-000-0000"></Props>
+
+
+    </div>
+  );
+}
+
+export default App;
