80c5c42471d6fc1a2593dd8bb3415e49b8b125bf
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-02-29 16:47:04 +0530

    Modern React With This bind in fat arrow function

---
+++ DAY 53/practice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Comp from './Comp';
+
+function App() {
+  return (
+    <div className="App">
+      <Comp></Comp>
+    </div>
+  );
+}
+
+export default App;
---
+++ DAY 53/practice/src/Comp.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Comp extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    state={
+        
+    }
+
+    handleChange=(event) => {
+
+    }
+    
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Comp
