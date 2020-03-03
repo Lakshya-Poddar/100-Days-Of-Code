a97cbda4cd83aa0327edd1cf8b445befe793d479
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-03-04 00:27:10 +0530

    Styling With CSS

---
+++ DAY 57/practice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <p>How are you???</p>
+      <h2>THANK YOU</h2>
+    </div>
+  );
+}
+
+export default App;
