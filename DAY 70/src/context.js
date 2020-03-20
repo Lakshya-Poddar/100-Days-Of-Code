b89d06a0052c5b0647bc4d8f613d23bbfe0f5aaf
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-03-20 18:54:14 +0530

    Online data fetching from contentful

--- DAY 70/src/context.js
+++ DAY 70/src/context.js
@@ -26,6 +26,31 @@ getRoom=(slug)=>{
     return room
 }
 
+
+
+// getData = async () =>{
+//     try{
+//         let response= await Client.getEntries({content_type:"beachResortRoom"})
+//         let rooms = this.formatData(response.items)
+//     let featuredRooms=rooms.filter(room => room.featured ===true)
+//     let maxPrice=Math.max(...rooms.map(item =>item.price))
+//     let maxSize=Math.max(...rooms.map(item =>item.size))
+//     this.setState({
+//         rooms:rooms,
+//         featuredRooms:featuredRooms,
+//         sortedRooms:rooms,
+//         loading:false,
+//         price:maxPrice,
+//         maxPrice:maxPrice,
+//         maxSize:maxSize
+
+//     })
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
+
+
 componentDidMount(){
     let rooms = this.formatData(items)
     let featuredRooms=rooms.filter(room => room.featured ===true)
