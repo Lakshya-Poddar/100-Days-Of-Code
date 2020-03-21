e0b6ac2ebd3746601686b69b8f96358753adc2e4
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-03-21 21:33:21 +0530

    New Project E-Commerce

---
+++ DAY 75/src/components/ProductList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Product from './Product'
+export class ProductList extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             products:[]
+        }
+    }
+    
+
+    render() {
+        return (
+                <React.Fragment>
+                    <div className=
+                </React.Fragment>
+                //<Product></Product>
+         
+        )
+    }
+}
+
+export default ProductList
