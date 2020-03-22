d87c042fcd6430d667dbbd1fb9deafe022378b0e
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-03-22 14:29:34 +0530

    Continued with the E-Commerce Project

---
+++ DAY 76/src/components/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Details extends Component {
+    render() {
+        return (
+            <div>
+                <h3> HE LLO FROM DETAILS</h3>
+            </div>
+        )
+    }
+}
+
+export default Details
