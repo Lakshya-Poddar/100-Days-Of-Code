25ae1538af81b37a9fa84b252568aceac6192662
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-03-24 17:33:12 +0530

    Continued with E-Commerce Project

---
+++ DAY 78/src/components/Cart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h3>HELLO FROM CART</h3>
+            </div>
+        )
+    }
+}
+
+export default Cart
