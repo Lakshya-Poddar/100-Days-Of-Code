c2f911d917a8c7a92c53e537d5a1d1092ebdab42
Lakshya Poddar <43548762+Lakshya-Poddar@users.noreply.github.com>
2020-04-04 12:54:05 +0530

    Completed With the cart section, aboutus and the totalling part of the cart page
    Just  left up with the app to cart to be implemented in the menu.

---
+++ DAY 88/src/components/Cart.js
@@ -0,0 +1,46 @@
+import React,{useContext} from 'react'
+import {MenuContext} from '../context'
+import CartList from  './CartItems'
+import EmptyCart from './EmptyCart'
+import styled from 'styled-components'
+import Total from './Total'
+
+function Cart() {
+    const {cart} =useContext(MenuContext)
+
+    if(cart.length>0)
+    { return (
+        <React.Fragment>
+        <NAV className="container-fluid text-center d-none d-lg-block bg-dark ">
+        <Line></Line>
+            <div className="row text-uppercase  ">
+                <div className ="col-10 col-lg-2 mx-auto"><h3  className="font">title</h3></div>
+                <div className ="col-10 col-lg-2 mx-auto"><h3 className="font">image</h3></div>
+                <div className ="col-10 col-lg-2 mx-auto"><h3 className="font">price</h3></div>
+                <div className ="col-10 col-lg-2 mx-auto"><h3 className="font">count</h3></div>
+                <div className ="col-10 col-lg-2 mx-auto"><h3 className="font">total</h3></div>
+            </div>
+        </NAV>
+            {cart.map(item=><CartList item={item} key={item.id} />)}
+            <Total />
+        </React.Fragment>
+    )}
+    else
+    {
+        return <EmptyCart />
+    }
+}
+
+export default Cart
+
+
+const Line = styled.div `
+    width:100%;
+    height:1px;
+    background:#fff; 
+    margin-bottom:10px;
+`
+const NAV =styled.nav `
+    color:#fff;
+    padding-top:10px;
+`
